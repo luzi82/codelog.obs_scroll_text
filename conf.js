ecb3fadb8d0e9ded3eb9135339337e9053ec5553
luzi82 <luzi82@gmail.com>
2020-08-15 15:47:49 +0800

    Settings go conf.js

---
+++ conf.js
@@ -0,0 +1,14 @@
+var DISPLAY_TEXT_PARAM = {
+    TXT_LIST:[
+        "Text 1",
+        "Text 2",
+        "Text 3",
+    ],
+    TXT_SPACE:80,
+    SCREEN_WIDTH:1920,
+    SCREEN_HEIGHT:80,
+    COLOR:"black",
+    FONT_SIZE:"40pt",
+    FONT_FAMILY:"Microsoft JhengHei",
+    SCROLL_SPEED:120,
+};
